4e0878653c596fab7e4a169c316d575654505500
NessaV <51070640+NessaV@users.noreply.github.com>
2023-05-25 01:41:53 +1000

    Rough working text-typing specific messages
    Figured out how to change the value of a JavaScript variable

--- im-2310/a3/script.js
+++ im-2310/a3/script.js
@@ -3,9 +3,16 @@
 // EXAMPLE: Typewriter
 var i = 0;
 // var txt = 'Lorem ipsum typing effect!'; /* The text */
+
+var txt;
+
+function setTxt(hTMLtxt) {
+  txt = hTMLtxt;
+}
+
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
-function typeWriter(txt) {
+function typeWriter() {
   if (i < txt.length) {
     document.getElementById("demo").innerHTML += txt.charAt(i);
     i++;
