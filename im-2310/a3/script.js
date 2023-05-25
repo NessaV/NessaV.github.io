dc22395228a369e2b07383301252c6b94f3ff7d9
NessaV <51070640+NessaV@users.noreply.github.com>
2023-05-25 15:22:51 +1000

    Saving before trying new code
    Wanting to make a chatroom container, then chat message containers to go inside of that.

--- im-2310/a3/script.js
+++ im-2310/a3/script.js
@@ -5,16 +5,30 @@ var i = 0;
 // var txt = 'Lorem ipsum typing effect!'; /* The text */
 
 var txt;
+var id;
 
-function setTxt(hTMLtxt) {
-  txt = hTMLtxt;
+function setTxt(htmlTxt) {
+  txt = htmlTxt;
+}
+
+function setId(htmlId) {
+  id = htmlId;
+}
+
+function getId() {
+  return id;
+}
+
+function newTypewriterMessage(htmlId, htmlTxt) {
+  setId(htmlId);
+  setTxt(htmlTxt);
 }
 
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
+    document.getElementById(id).innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
