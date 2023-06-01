1ee78d06f0214aeb749530b511a6045bfad2aeb3
NessaV <51070640+NessaV@users.noreply.github.com>
2023-06-01 15:27:00 +1000

    Error in my logic for Hide/Show Chat Message
    Ahh my logic was wrong for the hide chat message function 'setDivDisplay'. None of the chat messages' display is set to "none", that's why none of the messages are getting hidden.

--- im-2310/a3/script.js
+++ im-2310/a3/script.js
@@ -6,6 +6,7 @@ var i = 0;
 
 var txt;
 var id;
+var divId;
 
 function setTxt(htmlTxt) {
   txt = htmlTxt;
@@ -17,6 +18,33 @@ function setId(htmlId) {
 
 function getId() {
   return id;
+}
+
+// Show chat message
+/* REF 1/6/23
+    https://stackoverflow.com/questions/4528085/toggle-show-hide-div-with-button
+*/
+function setDivDisplay(htmlDivId) {
+  divId = htmlDivId;
+  // (document.getElementById(divId)).style.display = 'none';
+
+  /* IF display is set to 'none', THEN change display to 'block' */
+  var div = document.getElementById(divId);
+  if (div.style.display == "none") {
+    div.style.display == "flex";
+  } 
+  else {
+    div.style.display == "none";
+  }
+
+  // var div = document.getElementById(divId);
+  //   if (div.style.display !== 'none') {
+  //       div.style.display = 'none';
+  //   }
+  //   else {
+  //       div.style.display = 'block';
+  //   }
+
 }
 
 function newTypewriterMessage(htmlId, htmlTxt) {
@@ -33,5 +61,10 @@ function typeWriter() {
     document.getElementById(id).innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
-  }
+  } 
+  // Show next message, once done.
+  /*  */
+
+  // Testing with current message id?
+  // setDivDisplay("message-2");
 }
