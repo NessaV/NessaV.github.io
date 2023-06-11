d59f1d863cb97a31678426b65ce9eeb2ee1bf974
NessaV <51070640+NessaV@users.noreply.github.com>
2023-06-11 20:38:53 +1000

    Typewriter can only run once per message

--- im-2310/a3/script.js
+++ im-2310/a3/script.js
@@ -1,13 +1,15 @@
-
-
-// EXAMPLE: Typewriter
 var i = 0;
-// var txt = 'Lorem ipsum typing effect!'; /* The text */
 
 var txt;
 var id;
 var divId;
 var div; /* temp storage, for grabbing element by ID */
+
+var typeSpeed = 10; /* The speed/duration of the effect in milliseconds */
+var currentMessageId;
+var nextMessageId;
+
+var isTyped; /* boolean check */
 
 function setTxt(htmlTxt) {
   txt = htmlTxt;
@@ -32,13 +34,12 @@ function setDivDisplay(htmlDivId) {
 
   /* IF display is set to 'none', THEN change display to 'block' */
   div = document.getElementById(divId);
-    if (div.style.display !== 'none') {
-        div.style.display = 'none';
-    }
-    else {
-        div.style.display = 'flex';
-    }
-
+  if (div.style.display !== 'none') {
+    div.style.display = 'none';
+  }
+  else {
+    div.style.display = 'flex';
+  }
 }
 
 
@@ -51,16 +52,33 @@ function setDivDisplay(htmlDivId) {
   ELSE for all other cases, just set it to 'flex'     i.e. show the message
     i.e. set it to the preferred style of 'flex'
 */
+// function hideChatMessage(htmlDivId) {
+//   divId = htmlDivId;
+//   div = document.getElementById(divId);
+//     if (div.style.display !== 'none') {
+//         div.style.display = 'none';
+//     }
+//     else {
+//       // makes it a toggle button, the more you click on it
+//         // div.style.display = 'flex';
+//     }
+// }
+
+/* Using the 'hidden' class to keep messages hidden
+  REF: https://www.w3schools.com/howto/howto_js_add_class.asp
+*/
 function hideChatMessage(htmlDivId) {
   divId = htmlDivId;
   div = document.getElementById(divId);
-    if (div.style.display !== 'none') {
-        div.style.display = 'none';
-    }
-    else {
-      // makes it a toggle button, the more you click on it
-        // div.style.display = 'flex';
-    }
+  div.classList.add("hidden");
+}
+
+function showChatMessage(htmlDivId) {
+  divId = htmlDivId;
+  div = document.getElementById(divId);
+  console.log(divId);
+  div.classList.remove("hidden");
+  console.log(divId);
 }
 
 /* 
@@ -70,39 +88,77 @@ function hideChatMessage(htmlDivId) {
     THEN set it to 'flex'  i.e. show the message
   ELSE for all other cases, set it to 'none'   i.e. hide the message
 */
-function showChatMessage(htmlDivId) {
-  divId = htmlDivId;
-  div = document.getElementById(divId);
-    if (div.style.display !== 'flex') {
-        div.style.display = 'flex';
-    }
-    else {
-      // makes it a toggle button, the more you click on it
-        // div.style.display = 'none';
-    }
+// function showChatMessage(htmlDivId) {
+//   divId = htmlDivId;
+//   div = document.getElementById(divId);
+//     if (div.style.display !== 'flex') {
+//         div.style.display = 'flex';
+//     }
+//     else {
+//       // makes it a toggle button, the more you click on it
+//         // div.style.display = 'none';
+//     }
+// }
+
+/*
+  Get the next message ID, using the current message ID
+  ID format is: message-1, message-2, message-3, ...
+ */
+function getNextMessageId(currentMessageId) {
+  // if indexing is from 0, then we get the second string with index 1 --> the number
+  var currentNumber = currentMessageId.split("-")[1];
+  // make sure we are doing 'addition' on numbers and not appending a string
+  var nextNumber = parseInt(currentNumber) + 1;
+  return "message-" + nextNumber;
 }
 
-function newTypewriterMessage(htmlId, htmlTxt, clickedButton) {
+
+function newTypewriterMessage(htmlId, htmlTxt, currentMessageId, isTyped, clickedButton) {
   setId(htmlId);
   setTxt(htmlTxt);
 
-  i=0;
-  typeWriter();
-  clickedButton.onclick = "";
+  // run Typewriting effect
+  i = 0;
+  if (typeWriter()) {
+    console.log(typeWriter());
+    isTyped = true;
+  }
+
+  // get ID of next message
+  nextMessageId = getNextMessageId(currentMessageId);
   console.log(clickedButton);
+
+   // not sure how it works, but it stops the Typewriter from running more than once
+  clickedButton.onclick = "";
+ 
+  console.log("clickedButton = " + clickedButton);
+  console.log(clickedButton);
+  
+
+  // if message is completely Typed out
+  if (isTyped == true) {
+    console.log('is Typed is true');
+
+    // hides current message
+    // hideChatMessage(currentMessageId);
+    console.log('hide current Message');
+
+    // show next message
+    showChatMessage(nextMessageId);
+    console.log('show next Message');
+
+  } else if (isTyped == false) {
+    console.log('is Typed is false');
+    
+  }
 }
 
-var speed = 10; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById(id).innerHTML += txt.charAt(i);
     i++;
-    setTimeout(typeWriter, speed);
+    setTimeout(typeWriter, typeSpeed);
   } 
-  // Show next message, once done.
-  /*  */
-
-  // Testing with current message id?
-  // setDivDisplay("message-2");
+  return true;
 }
